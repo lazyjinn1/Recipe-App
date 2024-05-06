a167f0843bca8fc505d64480b8ebf66a577aed23
lazyjinn1 <joshjbarba@gmail.com>
2024-05-06 12:59:28 -0700

    update

--- recipe_app/recipe_app/settings.py
+++ recipe_app/recipe_app/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recipe_app.recipes'
+    'recipe_app.recipe_app.recipes'
 ]
 
 MIDDLEWARE = [
