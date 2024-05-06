1cad7716025ce9bb36c15149b5234f802c6bc654
lazyjinn1 <joshjbarba@gmail.com>
2024-05-06 12:45:26 -0700

    Update settings.py

--- recipe_app/recipe_app/settings.py
+++ recipe_app/recipe_app/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url # type: ignore
-import sys
-print(sys.path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recipes'
+    'recipe_app.recipes'
 ]
 
 MIDDLEWARE = [
