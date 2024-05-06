74033888f37e6721fa74d5f23b796b515fcce28a
lazyjinn1 <joshjbarba@gmail.com>
2024-05-06 14:12:55 -0700

    test

--- recipe_app/recipes/urls.py
+++ recipe_app/recipes/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'recipe_app.recipes'
+app_name = 'recipes'
 
 urlpatterns = [
     path('home', views.RecipeListView.as_view(), name='home'),
