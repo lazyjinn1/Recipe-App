63a0fe0d5bc62ac9d284fd86952a4790d829c8c0
lazyjinn1 <joshjbarba@gmail.com>
2024-05-06 17:08:49 -0700

    Update settings.py

--- recipe_app/settings.py
+++ recipe_app/settings.py
@@ -141,3 +141,11 @@ LOGIN_URL = '/login/'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
